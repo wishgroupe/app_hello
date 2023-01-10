ebbdd7e163ceb4f50dc8915f1072d1900d8ca03c
BoondManager-PS <122386982+BoondManager-PS@users.noreply.github.com>
2023-01-10 17:36:11 +0100

    Update init.php

--- init.php
+++ init.php
@@ -1,5 +1,5 @@
 <?php
 
-define('APP_KEY', '');
-define('APP_REFERENCE', '');
-define('APP_CODE', '');
+define('APP_KEY', ''); // Obligatoire, à récupérer dans la page de configuration de votre App (Administration / Espace Développeurs)
+// define('APP_REFERENCE', ''); // Legacy
+// define('APP_CODE', ''); // Uniquement si vous souhaitez utiliser un code d'installation 
